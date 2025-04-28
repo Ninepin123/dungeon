a5753e658e901d3b753aa8069db3ee22675294a0
Ninepin123 <pin45509951@gmail.com>
2025-04-28 16:54:59 +0800

    測試註解

--- src/main/java/me/ninepin/dungeonSystem/Dungeon/DungeonInteractListener.java
+++ src/main/java/me/ninepin/dungeonSystem/Dungeon/DungeonInteractListener.java
@@ -9,7 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-
+//多加了一個註解
 public class DungeonInteractListener implements Listener {
 
     private final DungeonSystem plugin;
